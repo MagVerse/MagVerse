2af9c3f4e760a745a52029a7088d6cd0812db3ad
Sj1218305 <jainshobit0@gmail.com>
2020-09-19 13:16:20 +0530

    reform dir structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,58 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: './src/javascripts/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-     module: {
-     rules: [
+  devtool: 'inline-source-map',
+   devServer: {
+     contentBase: './dist',
+   },
+  module: {
+    rules: [
        {
-         test: /\.s[ac]ss$/index,
+         test: /\.s[ac]ss$/i,
          use: [
            // Creates `style` nodes from JS strings
-           'style-loader',
+            'style-loader',{
             // Translates CSS into CommonJS
-           'css-loader',
-            // Compiles Sass to CSS
-           'sass-loader',
-         ],
-       },
-     ],
-   },
+            loader: 'css-loader',
+            options: { importLoaders: 1 },
+            },
+            {
+              loader:'postcss-loader',
+              options:{
+                postcssOptions: {
+                  plugins: [
+                  [
+                  'autoprefixer',
+                  {
+                      //options
+                  }
+                  ],  
+                  ],
+                },
+              },
+            },
+                      //    {
+          //   // Loader for webpack to process CSS with PostCSS
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     plugins: function () {
+          //       return [
+          //       require('precss'),
+          //         require('autoprefixer')
+          //       ];
+          //     }
+          //   }
+          // },
+          // Compiles Sass to CSS
+          'sass-loader',
+         ]
+       }
+     ]
+   }
 };
